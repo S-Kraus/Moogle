9610fd9767ee9ddb1f419b99c8fded3cc0370132
dump66 <andyschultze@hotmail.com>
2017-11-21 08:28:33 +0100

    Schnittstellenbeschreibung hinzugefügt

--- MyNer/src/NERDemo.java
+++ MyNer/src/NERDemo.java
@@ -10,33 +10,10 @@ import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.util.Triple;
 
 /**
- * This is a demo of calling CRFClassifier programmatically.
- * <p>
- * Usage:
- * {@code java -mx400m -cp "*" NERDemo [serializedClassifier [fileName]] }
- * <p>
- * If arguments aren't specified, they default to
- * classifiers/english.all.3class.distsim.crf.ser.gz and some hardcoded sample
- * text. If run with arguments, it shows some of the ways to get k-best
- * labelings and probabilities out with CRFClassifier. If run without arguments,
- * it shows some of the alternative output formats that you can get.
- * <p>
- * To use CRFClassifier from the command line:
- * </p>
- * <blockquote>
- * {@code java -mx400m edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier [classifier] -textFile [file] }
- * </blockquote>
- * <p>
- * Or if the file is already tokenized and one word per line, perhaps in a
- * tab-separated value format with extra columns for part-of-speech tag, etc.,
- * use the version below (note the 's' instead of the 'x'):
- * </p>
- * <blockquote>
- * {@code java -mx400m edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier [classifier] -testFile [file] }
- * </blockquote>
- *
- * @author Jenny Finkel
- * @author Christopher Manning
+ * InitSets einmalig für jede Datei ausführen, damit die Sets gefüllt werden.
+ * Danach kann auf die Getter Funktionen zugegriffen werden.
+ * Bei Hauptprogramm als VM Parameter folgendes mitgeben:
+ * -mx400m -cp "*"
  */
 
 public class NERDemo {
@@ -45,7 +22,10 @@ public class NERDemo {
 	private LinkedHashSet<String> orgs = new LinkedHashSet<String>();
 
 	private LinkedHashSet<String> pers = new LinkedHashSet<String>();
-
+	/**
+	 * 
+	 * @param file Dateiname inkl. Pfad
+	 */
 	public void initSets(String file) throws ClassCastException, ClassNotFoundException, IOException {
 
 		String serializedClassifier = "edu/stanford/nlp/models/ner/german.conll.hgc_175m_600.crf.ser.gz";
