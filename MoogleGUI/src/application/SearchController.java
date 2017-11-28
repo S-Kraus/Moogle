5c021568488ddabf003887b797a502a6094eea67
Stephan.Pautz <Stephan.Pautz@VI2011505>
2017-11-28 09:14:53 +0100

    Der Beleidiger Teil II

--- MoogleGUI/src/application/SearchController.java
+++ MoogleGUI/src/application/SearchController.java
@@ -1,12 +1,6 @@
 package application;
 
-import java.beans.EventHandler;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
-import com.sun.javafx.scene.EventHandlerProperties;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -15,54 +9,65 @@ import javafx.scene.control.Button;
 //import javafx.scene.control.TextField;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
- 
+
 public class SearchController {
-	
-	ObservableList<String> choiceboxList = FXCollections.observableArrayList("Volltextsuche", "Personensuche", "Organisationssuche");
-	
+
+	ObservableList<String> choiceboxList = FXCollections.observableArrayList("Volltextsuche", "Personensuche",
+			"Organisationssuche");
+
 	@FXML
 	ChoiceBox<String> choiceBox;
-	
+
 	@FXML
 	TextField suchtextfeld;
-//	
-//	@FXML
-//	DatePicker zeitraumvon;
-//	
-//	@FXML
-//	DatePicker zeitraumbis;
-	
-	@FXML 
-	public void initialize(){
+	//
+	// @FXML
+	// DatePicker zeitraumvon;
+	//
+	// @FXML
+	// DatePicker zeitraumbis;
+
+	@FXML
+	public void initialize() {
 		choiceBox.getItems().remove(choiceBox.getItems());
 		choiceBox.getItems().addAll(choiceboxList);
 		choiceBox.getSelectionModel().select(0);
 	}
-		
+
 	@FXML
 	Button suchanfrage;
-	
+
 	@FXML
-	protected void buttonPressed(){
+	protected void buttonPressed() {
 		String text = suchtextfeld.getText();
 		suchtextfeld.clear();
-		//suchtextfeld.setText("Selber " + text);
-		
-		//Der Beleidiger
+		// suchtextfeld.setText("Selber " + text);
+
+		// Der Beleidiger
 		HashMap map = new HashMap();
-			
+
 		map.put(new String("arsch"), new String("Selber Arsch. Du riesen Schildkröte!"));
 		map.put(new String("penner"), new String("Dei Muadda, Buarsche!!!"));
 		map.put(new String("moogle"), new String("Ja. Und jetzt?"));
-		//suchtextfeld.setText((String)map.get(text));
-		
-		 Iterator it = map.entrySet().iterator();
-	        while (it.hasNext()) {
-	          Map.Entry entry = (Map.Entry)it.next();
-	          if(entry.getKey().equals(text.toLowerCase())){
-	              suchtextfeld.setText((String)entry.getValue());
-	          }
+		map.put(new String("suche"), new String("Na klar. Such dich selber Junge!"));
+		map.put(new String("rene"), new String("Wir lieben dich (deine drei kleinen Schweinchen!)"));
+		map.put(new String("simon"), new String("Hallo Simon! Willkommen in deiner Selbsthilfegruppe"));
+		map.put(new String("andreas"), new String("Schön das du da bist und nicht hier :-)"));
+		map.put(new String("stephan"), new String("Was soll mann dazu sagen? Du geiler Typ!"));
 
+		// Variante mit Iterator
+//		Iterator it = map.entrySet().iterator();
+//		while (it.hasNext()) {
+//			Map.Entry entry = (Map.Entry) it.next();
+//			if (entry.getKey().equals(text.toLowerCase())) {
+//				suchtextfeld.setText((String) entry.getValue());
+//			}
+//
+//		}
+		
+		// Variante ohne Iterator
+		if (map.containsKey(text)){
+			suchtextfeld.setText((String) map.get(text));
+		}
 	}
-}
 }
