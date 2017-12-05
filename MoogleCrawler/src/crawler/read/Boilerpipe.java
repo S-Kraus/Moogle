baa77c14ca7002c188f254c87124a5c9d8b339a6
Simon.Kraus <Simon.Kraus@VI201513>
2017-12-05 11:22:12 +0100

    Alle Handler

---
+++ MoogleCrawler/src/crawler/read/Boilerpipe.java
@@ -0,0 +1,43 @@
+package crawler.read;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import de.l3s.boilerpipe.BoilerpipeProcessingException;
+import de.l3s.boilerpipe.document.TextDocument;
+import de.l3s.boilerpipe.extractors.ArticleExtractor;
+import de.l3s.boilerpipe.sax.BoilerpipeSAXInput;
+import de.l3s.boilerpipe.sax.HTMLFetcher;
+
+public class Boilerpipe {
+	
+	public static String useBoilerpipe(String urlString){
+		URL url;
+		try {
+			url = new URL(urlString);
+			final InputSource is = HTMLFetcher.fetch(url).toInputSource();
+			final BoilerpipeSAXInput in = new BoilerpipeSAXInput(is);
+			final TextDocument doc = in.getTextDocument();
+			return ArticleExtractor.INSTANCE.getText(doc);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return null;
+		} catch (BoilerpipeProcessingException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		} catch (SAXException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		
+	}
+
+}
--- MoogleCrawler/src/crawler/tests/ReadTestNew.java
+++ MoogleCrawler/src/crawler/tests/ReadTestNew.java
@@ -1,12 +1,30 @@
 package crawler.tests;
 
+import java.io.IOException;
 import java.util.List;
 
 import crawler.model.Message;
+import crawler.read.Boilerpipe;
 import crawler.read.RSSHandler4Players;
 import crawler.read.RSSHandlerChip;
+import crawler.read.RSSHandlerGamepro;
+import crawler.read.RSSHandlerGamestar;
+import crawler.read.RSSHandlerGiga;
+import crawler.read.RSSHandlerGolem;
+import crawler.read.RSSHandlerIgn;
 import crawler.read.RSSParserNew;
-
+import ner.NERDemo;
+/**
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * 			Achtung!
+ * Program dauert SEHR LANGE (mehrere Minuten)
+ * -Es liest alle Seiten
+ * -holt sich mit der boilerpipe den Artikeltext
+ * -lässt danach noch das NER-Tool über den Text laufen
+ * 
+ * @author Simon.Kraus
+ *
+ */
 public class ReadTestNew {
 	
 	final static String fourplayers = "http://feeds.4players.de/Allgemein/articles/-/rss.xml";
@@ -17,20 +35,98 @@ public class ReadTestNew {
 	final static String golem = "https://rss.golem.de/rss.php?tp=games&feed=RSS2.0";
 	final static String ign = "http://de.ign.com/news.xml";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassCastException, ClassNotFoundException, IOException {
+		
+		long a = System.currentTimeMillis();
 		
 		RSSParserNew parser4Players = new RSSParserNew(fourplayers,"4PlayersXSD", new RSSHandler4Players());
 		List<Message> list4Players = parser4Players.readFeed();
 		for(Message message: list4Players) {
-			System.out.println(message);
+			message.setExtractedText(Boilerpipe.useBoilerpipe(message.getGuid()));
+//			NERDemo ner = new NERDemo();
+//			ner.initSets(message.getExtractedText());
+//			message.setOrganisationen(ner.getOrgs().toString());
+//			message.setPersonen(ner.getPers().toString());
+		
 		}
+		System.out.println("Finished 4Players");
 		
-//		RSSParserNew parserChip = new RSSParserNew(chip,"ChipXSD", new RSSHandlerChip());
-//		List<Message> listChip = parserChip.readFeed();
-//		for(Message message: listChip) {
-//			System.out.println(message);
-//		}
 		
+		RSSParserNew parserChip = new RSSParserNew(chip,"ChipXSD", new RSSHandlerChip());
+		List<Message> listChip = parserChip.readFeed();
+		for(Message message: listChip) {
+			message.setExtractedText(Boilerpipe.useBoilerpipe(message.getGuid()));
+			NERDemo ner = new NERDemo();
+			ner.initSets(message.getExtractedText());
+			message.setOrganisationen(ner.getOrgs().toString());
+			message.setPersonen(ner.getPers().toString());
+		}
+		System.out.println("Finished Chip");
+		
+		RSSParserNew parserGamepro = new RSSParserNew(gamepro,"GameproXSD", new RSSHandlerGamepro());
+		List<Message> listGamepro = parserGamepro.readFeed();
+		for(Message message: listGamepro) {
+			message.setExtractedText(Boilerpipe.useBoilerpipe(message.getGuid()));
+			NERDemo ner = new NERDemo();
+			ner.initSets(message.getExtractedText());
+			message.setOrganisationen(ner.getOrgs().toString());
+			message.setPersonen(ner.getPers().toString());
+		}
+		System.out.println("Finished Gamepro");
+		
+		RSSParserNew parserGamestar = new RSSParserNew(gamestar,"GamestarXSD", new RSSHandlerGamestar());
+		List<Message> listGamestar = parserGamestar.readFeed();
+		for(Message message: listGamestar) {
+			message.setExtractedText(Boilerpipe.useBoilerpipe(message.getGuid()));
+			NERDemo ner = new NERDemo();
+			ner.initSets(message.getExtractedText());
+			message.setOrganisationen(ner.getOrgs().toString());
+			message.setPersonen(ner.getPers().toString());
+		}
+		System.out.println("Finished Gamestar");
+		
+		RSSParserNew parserGiga = new RSSParserNew(giga,"GigaXSD", new RSSHandlerGiga());
+		List<Message> listGiga = parserGiga.readFeed();
+		for(Message message: listGiga) {
+			message.setExtractedText(Boilerpipe.useBoilerpipe(message.getGuid()));
+			NERDemo ner = new NERDemo();
+			ner.initSets(message.getExtractedText());
+			message.setOrganisationen(ner.getOrgs().toString());
+			message.setPersonen(ner.getPers().toString());
+		}
+		System.out.println("Finished Giga");
+		
+		RSSParserNew parserGolem = new RSSParserNew(golem,"GolemXSD", new RSSHandlerGolem());
+		List<Message> listGolem = parserGolem.readFeed();
+		for(Message message: listGolem) {
+			message.setExtractedText(Boilerpipe.useBoilerpipe(message.getGuid()));
+			NERDemo ner = new NERDemo();
+			ner.initSets(message.getExtractedText());
+			message.setOrganisationen(ner.getOrgs().toString());
+			message.setPersonen(ner.getPers().toString());
+		}
+		System.out.println("Finished Golem");
+		
+		RSSParserNew parserIgn = new RSSParserNew(ign,"IgnXSD", new RSSHandlerIgn());
+		List<Message> listIgn = parserIgn.readFeed();
+		for(Message message: listIgn) {
+			message.setExtractedText(Boilerpipe.useBoilerpipe(message.getGuid()));
+			NERDemo ner = new NERDemo();
+			ner.initSets(message.getExtractedText());
+			message.setOrganisationen(ner.getOrgs().toString());
+			message.setPersonen(ner.getPers().toString());
+
+
+		}
+		System.out.println("Finished Ign");		
+		System.out.println("Finished All");
+		long b = System.currentTimeMillis() - a;
+		System.out.println(b + " Millisekunden");
+		System.out.println(b/1000 + " Sekunden");
+	
+	
+	
 	}
+	
 
 }
